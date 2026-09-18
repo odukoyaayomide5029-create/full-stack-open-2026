188bfcdbb75ddf90abe0bcb52f20133e2366cfe5
Ayo Odukoya <odukoyaayomide5029@gmail.com>
2026-09-18 07:56:09 +0100

    added eslint

--- part3/index.js
+++ part3/index.js
@@ -43,15 +43,15 @@ morgan.token('body', (request) => {
 })
 app.use(morgan(':method :url :status :body'))
 
-app.get('/', (req, res) => {
+app.get('/', (req, res,) => {
   res.status(200).send('Server is running');
 });
-app.get('/api/persons',(request,response)=>{
+app.get('/api/persons',(request,response,next)=>{
 Person.find({}).then(persons => {
     response.json(persons)
   }).catch(error => next(error))
 })
-app.get('/api/persons/:id',(request,response)=>{
+app.get('/api/persons/:id',(request,response,next)=>{
    Person.findById(request.params.id)
     .then(person => {
       if (person) {
