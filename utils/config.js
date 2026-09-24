68bcee98fd1a453b65e74ec88fa2fcbeecd6618f
Ayo Odukoya <odukoyaayomide5029@gmail.com>
2026-09-24 07:29:03 +0100

    Keep part4 files under part4 directory

--- utils/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const url = process.env.NODE_ENV === 'test'
-  ? process.env.TEST_MONGODB_URI
-  : process.env.MONGODB_URI
-const PORT = process.env.PORT || 3003
-const SECRET = process.env.SECRET
-module.exports = { url, PORT,SECRET }
